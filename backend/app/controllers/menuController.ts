4d593e845c70798574ded3658cb1c08b529f11dd
Ichiroki <fahrezirizqiawan12649@gmail.com>
2023-11-29 10:26:30 +0700

    merge to master

--- backend/app/controllers/menuController.ts
+++ backend/app/controllers/menuController.ts
@@ -19,6 +19,40 @@ export const menuGet = async (req, res, next) => {
    }
 }
 
+export const addRating = async (userId: string, productId: string, rating: number, feedback: string) => {
+   var today = new Date().toISOString();
+
+   try {
+      const product = await prisma.products.findUnique({
+         where: {
+            id: productId
+         }
+      })
+      const user = await prisma.users.findUnique({
+         where: {
+            id: userId
+         }
+      })
+
+      if(!product || !user) {
+         throw new Error('Product or user tidak ditemukan')
+      }
+
+      await prisma.productrating.create({
+         data: {
+            productId,
+            userId,
+            rating,
+            feedback,
+            created_at: today
+         }
+      })
+   } catch(e) {
+      console.log("Internal server error, please wait " + e)
+      throw e
+   }
+}
+
 // For API Only
 
 export const menuGetAPI = async (req, res) => {
